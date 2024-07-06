bdea94f0eec55f0fbd8feb3d34f0d63f1b67d210
Nikita Polianskii <157965326+emberFrog@users.noreply.github.com>
2024-07-06 19:56:38 +0300

    mid2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,42 @@ module.exports = {
 		'import/newline-after-import': 'error',
 		'import/no-duplicates': 'error',
 	},
+	overrides: [
+		{
+			files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+			rules: {
+				'simple-import-sort/imports': [
+					'error',
+					{
+						groups: [
+							// Packages `react` related packages come first.
+							['^react', '^\\w', '^@hookform', '^@radix-ui'],
+							// npm packages
+							// Anything that starts with a letter (or digit or underscore), or `@` followed by a letter.
+							// ['^\\w'],
+							// Internal packages.
+							['^@store(/.*|$)'],
+							['^@components(/.*|$)'],
+							['^@ui(/.*|$)'],
+							['^@lib(/.*|$)'],
+							['^@pages(/.*|$)'],
+							['^@utils(/.*|$)'],
+							['^@hooks(/.*|$)'],
+							['^@services(/.*|$)'],
+							// Side effect imports.
+							['^\\u0000'],
+							// Parent imports. Put `..` last.
+							['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+							// Other relative imports. Put same-folder imports and `.` last.
+							['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+							// Style imports.
+							['^.+\\.?(css)$'],
+						],
+					},
+				],
+			},
+		},
+	],
 }
 
 export default [
